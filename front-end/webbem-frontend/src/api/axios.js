13367fb25ef8bc0e2c2b3065aa5a5c0321026a69
siaksi <siaksibemstikomcrb@gmail.com>
2026-02-07 09:45:22 +0700

    update axios

--- front-end/webbem-frontend/src/api/axios.js
+++ front-end/webbem-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'http://siaksi.stikompoltekcirebon.ac.id/api', 
   withCredentials: true,
   timeout: 60000,
 });
